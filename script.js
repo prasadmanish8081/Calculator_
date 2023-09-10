c2fde0d5f1a798cdd80e6182a3108953fe52b8b3
Manish Prasad <136253028+prasadmanish8081@users.noreply.github.com>
2023-09-10 22:13:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+
+let str = "";
+let array = Array.from(buttons);
+array.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            str = eval(str);
+            input.value = str;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            str = "";
+            input.value = str;
+        }
+
+        else if(e.target.innerHTML == 'DEL'){
+            str = str.substring(0, str.length-1);
+            input.value = str;
+        }
+        else{
+            str +=e.target.innerHTML;
+            input.value = str;
+        }
+    })
+})
+
+
